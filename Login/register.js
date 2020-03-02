80f7e3dd970013c67f06ffc984db1ca4e889025b
Shiny <32192009+shinyjks@users.noreply.github.com>
2020-03-02 15:21:45 +0530

    Update register.js

--- Login/register.js
+++ Login/register.js
@@ -11,53 +11,42 @@ function Validation()
 	var mobno = document.form1.mobno.value;
 	var userid = document.form1.userid.value;
 	var address = document.form1.address.value;
-	var flag = 0;
-	if(username.value.match(usernameFormat) && username.length >= 6)
+
+	if(username == "" && username != usernameFormat && username.length < 6)
 	{
-		continue;
-	}
-	else{
 		alert("Enter the correct username format (only letters with minimum of 6 characters)");
-		flag += 1;
-	}
-	if(password.value.match(passfor) && password.length >= 6)
-	{
-		continue;
-	}
-	else{
-		alert("Enter the correct password format (only alphanumeric with minimum of 6 characters");
-		flag += 1;
-	}
-	if(email.value.match(mailformat))
-	{
-		continue;
-	}
-	else{
-		alert("Enter the correct email id");
-		flag += 1;
-	}
-	if(mobno.value.match(mobfor) && mobno.length == 10)
-	{
-		continue;
-	}
-	else{
-		alert("Enter the correct mobile number");
-		flag += 1;
-	}
-	if(userid.value.match(useridFormat) && userid.length >= 6 && userid.length <= 8)
-	{
-		continue;
-	}
-	else
-	{
-		alert("Enter correct userid");
-	}
-	if(flag == 0)
-	{
-		return true;
-	}
-	else
-	{
+		document.form1.username.focus();
 		return false;
 	}
+	if(password == "" && password != passfor && password.length < 6)
+	{
+		alert("Enter the correct password format (only alphanumeric with minimum of 6 characters");
+		document.form1.password.focus();
+		return false;
+	}
+	if(email == "" && email != mailformat)
+	{
+		alert("Enter the correct email id");
+		document.form1.email.focus();
+		return false;
+	}
+	if(mobno == "" && mobno != mobfor && mobno.length != 10)
+	{
+		alert("Enter the correct mobile number");
+		document.form1.mobno.focus();
+		return false;
+	}
+	if(userid == "" && userid != useridFormat && userid.length < 6 && userid.length > 8)
+	{
+		alert("Enter correct userid");
+		document.form1.userid.focus();
+		return false;
+	}
+	if(address == "")
+	{
+		alert("Enter the address");
+		document.form1.address.focus();
+		return false;
+	}
+	return (true);
 }
